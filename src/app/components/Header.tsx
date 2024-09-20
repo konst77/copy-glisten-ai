c2122b957f8b145436075a74dcf9e74d244f8b5d
konst77 <rayyoung.moon@gmail.com>
2024-09-20 13:23:52 -0700

    Initial Commit

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/app/components/Navbar";
+import { createClient } from "@/prismicio";
+
+export default async function Header() {
+  const client = createClient();
+  const settings = await client.getSingle("settings")
+  
+  return (
+    <div>
+      <Navbar settings={settings} />
+    </div>
+  )
+}
